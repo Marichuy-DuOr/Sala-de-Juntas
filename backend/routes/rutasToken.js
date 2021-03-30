9f8893dafaac85785ceb72b901e8d9d887736743
Mary Chuy <maria.0299.ornelas@gmail.com>
2021-03-30 03:03:07 -0600

    Querys para las consultas de salas, creacion de reservacion, modificacion de sala segun disponibilidad y eliminacion de reservaciones

--- backend/routes/rutasToken.js
+++ backend/routes/rutasToken.js
@@ -42,4 +42,62 @@ router.get('/salas', [], (req, res) => {
     }))
 });
 
+router.get('/sala/:id', [
+    param('id').not().isEmpty().isNumeric(),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+    let id = req.params.id;
+    user.getIdSala(connection, id, (data => {
+        res.json(data);
+    }))
+});
+
+router.get('/verReservaciones/:id', [
+    param('id').not().isEmpty().isNumeric(),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+    let id = req.params.id;
+    user.getReservacionIdSala(connection, id, (data => {
+        res.json(data);
+    }))
+});
+
+router.post('/reservacion', (req, res) => {
+    let body = req.body;
+    body.idUsuario = req.idUsuario;
+    user.createReservacion(connection, body, (data => {
+        res.json(data);
+    }));
+});
+
+router.put('/updSala', [], (req, res) => {
+    let body = req.body;
+    user.updateSala(connection, body, (data => {
+        res.json(data);
+    }))
+});
+
+router.delete('/reservacion/:id', [
+    param('id').not().isEmpty().isNumeric()
+], (req, res) => {
+    const errors = validationResult(req);
+    let params = req.params;
+    console.log(params);
+    if (!errors.isEmpty()) {
+        res.json({ success: false, err: JSON.stringify(errors) })
+        return
+    }
+    user.deleteReservacion(connection, params, (data => {
+        res.json(data);
+    }))
+});
+
 module.exports = router;
